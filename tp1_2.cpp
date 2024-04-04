57582cb4c6420d0d0fa8e40f1f71c59823b25c14
juancookz <jcavila383@gmail.com>
2024-04-03 21:58:22 -0300

    Ejercicio 4 C completado

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -5,7 +5,9 @@ int main()
     int numero, res;
     printf("Ingrese un numero:");
     scanf("%d",&numero);
+    printf("\nVariable recibida\nValor: %d\nDireccion: %p\n", numero, &numero);
     cuadradoNum(numero, &res);
+    printf("\nVariable recibida\nValor: %d\nDireccion: %p\n", res, &res);
     printf("El cuadrado del numero es: %d", res);
     return 0;
 }
