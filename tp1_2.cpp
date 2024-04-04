81bf3e6b9cad01102897c5a718eb33cca660b9c9
juancookz <jcavila383@gmail.com>
2024-04-04 01:07:22 -0300

    Ejercicio 4 E completado

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-void invertir(int *a, int *b);
+void orden(int *a, int *b);
 int main()
 {
     int a, b;
@@ -7,15 +7,18 @@ int main()
     scanf("%d", &a);
     printf("\nIngrese b:");
     scanf("%d", &b);
-    invertir(&a, &b);
-    printf("\na es: %d", a);
-    printf("\nb es: %d", b);
+    orden(&a, &b);
+    printf("\nMayor: %d", a);
+    printf("\nMenor: %d", b);
     return 0;
 }
-void invertir(int *a, int *b)
+void orden(int *a, int *b)
 {
-    int c;
-    c = *a;
-    *a = *b;
-    *b = c;
+    int num1 = *a;
+    int num2 = *b;
+    if (num1 < num2)
+    {
+        *b = num1;
+        *a = num2;
+    }
 }
