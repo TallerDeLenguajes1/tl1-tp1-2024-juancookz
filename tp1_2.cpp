dad2be54eae66286f23465a33956163cb6b36de8
juancookz <jcavila383@gmail.com>
2024-04-03 21:26:15 -0300

    Ejercicio 4 A completado

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -1,6 +1,13 @@
 #include <stdio.h>
+int cuadradoNum(int num);
 int main()
 {
-    /*Code*/
+    int numero;
+    printf("Ingrese un numero:");
+    scanf("%d",&numero);
+    printf("\nEl numero al cuadrado es %d", cuadradoNum(numero));
     return 0;
+}
+int cuadradoNum(int num){
+    return num*num;
 }
